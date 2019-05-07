4faeba973e5d9d8e78f38320cff8cf893b430f1a
LucasGalmeida <48827453+LucasGalmeida@users.noreply.github.com>
2019-05-07 13:08:45 -0300

    Tela de login
    Adicionado uma tela de login simples.

--- Grid.py
+++ Grid.py
@@ -2,13 +2,23 @@ import tkinter as tk
 
 janela = tk.Tk()
 
-lb1 = tk.Label(janela,text="teste")
-lb1.grid(row=20, column=10)
+lb1 = tk.Label(janela, text="Login:")
+lb2 = tk.Label(janela, text="Senha:")
 
-lb2 = tk.Label(janela,text="teste2")
-lb2.grid(row=21, column=11)
+ed1 = tk.Entry(janela)
+ed2 = tk.Entry(janela,show="*")
 
+bt1 = tk.Button(janela, text="Confirmar")
 
-janela.geometry("500x200+600+200")
+lb1.grid(row=0, column=0)
+lb2.grid(row=1, column=0)
+ed1.grid(row=0, column=1)
+ed2.grid(row=1, column=1)
+bt1.grid(row=2, column=1)
+
+janela.geometry("200x100+100+100")
 
 janela.mainloop()
+
+# stick > indica o sentido que o elemento estara na tela
+# rowspan e columnspan > Mescla linhas e colunas
